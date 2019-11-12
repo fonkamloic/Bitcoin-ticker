f56079b77a0b6f285095aacd881132ac1a869fd2
fonkamloic <fonkamloic@gmail.com>
2019-11-12 17:53:06 +0100

    fetching along useful data

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -4,8 +4,6 @@ import 'dart:convert';
 class NetworkHelper {
   Future<double> getData({String url}) async {
     http.Response data = await http.get(url);
-//    print(data.body);
-    dynamic decodeData = jsonDecode(data.body);
-    return decodeData['last'];
+    return jsonDecode(data.body)['last'];
   }
 }
